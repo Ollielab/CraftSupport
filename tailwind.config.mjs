73877071dbaf5bda8ac7eb7fda093d113fae147c
Ollielab <oliver.simons123@outlook.com>
2023-12-10 17:35:49 +0000

    1.0 release

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			scale: {
+				'200': '2.00',
+			}
+		},
 	},
 	plugins: [],
 }
