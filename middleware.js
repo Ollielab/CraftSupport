741dd3e4ba6b0c76dd99bef518e738fdd7295f01
Ollielab <oliver.simons123@outlook.com>
2023-12-10 21:35:01 +0000

    fix

--- middleware.js
+++ middleware.js
@@ -2,12 +2,15 @@ export const config = {
     // Only run the middleware on the admin route
     matcher: '/discord',
   };
+
+<script>function myFunction() {window.location.href = "https://exampleURL.com/"}</script>
   
   export default function middleware(request) {
     const url = new URL(request.url);
-    // You can retrieve IP location or cookies here.
     if (url.pathname === '/discord') {
       url.pathname = 'https://discord.com/invite/SP87wwm6DU';
     }
-    return Response.redirect(url);
+    myFunction()
   }
+
+  
