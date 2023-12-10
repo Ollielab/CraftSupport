5f748505b47751c716c717e1167f5ae10fe5351d
Ollielab <oliver.simons123@outlook.com>
2023-12-10 21:36:58 +0000

    test

--- middleware.js
+++ middleware.js
@@ -2,15 +2,13 @@ export const config = {
     // Only run the middleware on the admin route
     matcher: '/discord',
   };
-
-<script>function myFunction() {window.location.href = "https://exampleURL.com/"}</script>
-  
+ 
   export default function middleware(request) {
     const url = new URL(request.url);
     if (url.pathname === '/discord') {
       url.pathname = 'https://discord.com/invite/SP87wwm6DU';
     }
-    myFunction()
+    window.location.replace("http://www.w3schools.com");
   }
 
   
