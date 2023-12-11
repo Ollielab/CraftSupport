64808385c6c75d838e3f6d4489cd93d4f0f60c49
Ollielab <oliver.simons123@outlook.com>
2023-12-11 08:17:36 +0000

    removed review

--- middleware.js
+++ middleware.js
@@ -1,11 +1,9 @@
 export const config = {
-    // Only run the middleware on the admin route
     matcher: '/discord',
   };
   
   export default function middleware(request) {
     const url = new URL(request.url);
-    // You can retrieve IP location or cookies here.
     if (url.pathname === '/discord')
     return Response.redirect("https://discord.com/invite/SP87wwm6DU");
   }
