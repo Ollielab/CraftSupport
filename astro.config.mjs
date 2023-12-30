d8f94baa026fa2ab969cb340be2d00710dd1e0b2
Ollielab <oliver.simons123@outlook.com>
2023-12-30 23:59:41 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://craftsupport.net',
   integrations: [tailwind(), sitemap()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
