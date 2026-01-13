3a61d93c7556a0c013ad7130742ae80bf8747a41
Oliver Simons <oliver.simons123@outlook.com>
2026-01-13 01:26:44 +0000

    New generation of CraftSupport.

--- middleware.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  matcher: "/discord",
-};
-
-export default function middleware(request) {
-  const url = new URL(request.url);
-  if (url.pathname === "/discord")
-    return Response.redirect("https://discord.com/invite/SP87wwm6DU");
-}
