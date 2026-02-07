2ca572d2149a52d1ded999fff182e9be52f957c0
Oliver Simons <oliver.simons123@outlook.com>
2026-02-07 14:28:28 +0000

    chore: update dependencies and configuration for Cloudflare integration
    - Updated @astrojs/sitemap to version 3.7.0 and added @astrojs/cloudflare dependency.
    - Upgraded astro to version 5.17.1.
    - Removed @vercel/speed-insights from MainLayout.astro.
    - Added .assetsignore file to exclude worker and routes JSON files.
    - Created wrangler.jsonc for Cloudflare Workers configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from '@astrojs/vercel';
+
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://craftsupport.net",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sitemap()],
   output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: cloudflare(),
 });
